d8463f0eda1906a8e9b02e44e2133e6a9c07e144
Andrey Takhtamirov <andrey1229@icloud.com>
2021-02-25 01:03:44 -0500

    Added async file writing, log levels, time

--- Server/main.py
+++ Server/main.py
@@ -1,36 +1,90 @@
+import asyncio
+import os
 import socket
 import threading
+from datetime import datetime
+
+import aiofiles as aiof
+
+
+class ClientThread(threading.Thread):
+    def __init__(self, clientsocket):
+        threading.Thread.__init__(self)
+        self.csocket = clientsocket
+
+    def run(self):
+        data = self.csocket.recv(RECEIVE_BUFFER)
+        msg = data.decode()
+        server_response = process_message(msg)
+        self.csocket.send(bytes(server_response, "UTF-8"))
+        print("Client disconnected")
+
+
+def process_message(message):
+    # print("message received: " + message)
+    message_elements = message.split("|")
+
+    if len(message_elements) == 3:
+        message_elements[1] = set_log_level(int(message_elements[1]))
+        print("ID: ", message_elements[0])
+        print("Log Level: ", message_elements[1])
+        print("Log Message: ", message_elements[2])
+
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        try:
+            loop.run_until_complete(write_to_file("./logs/messages.log", message_elements))
+        finally:
+            loop.run_until_complete(loop.shutdown_asyncgens())
+            loop.close()
+
+        return "Write Successful."
+    else:
+        return "ERROR: Unknown log format."
+
+
+async def write_to_file(filename, message_elements):
+    current_time = datetime.utcnow().strftime(time_format)[:-3]
+    print(current_time)
+
+    if os.path.exists(filename):
+        file_permission = "a"
+    else:
+        file_permission = "w"
+
+    async with aiof.open(filename, file_permission) as out:
+        await out.write(current_time + " " + message_elements[0] + " " +
+                        message_elements[1] + " " +
+                        message_elements[2] + "\n")
+        await out.flush()
+
+
+def set_log_level(log_level):
+    return log_levels.get(log_level)
 
 
 def main():
-    class ClientThread(threading.Thread):
-        def __init__(self, client_address, clientsocket):
-            threading.Thread.__init__(self)
-            self.csocket = clientsocket
-
-        def run(self):
-            print("Connection from : ", client_address)
-            self.csocket.send(bytes("Connection to server established", 'utf-8'))
-            msg = ''
-            data = self.csocket.recv(2048)
-            msg = data.decode()
-            print("Message from client: ", msg)
-            print("Client disconnected ", client_address)
-            self.csocket.send(bytes(msg, 'UTF-8'))
-
-    LOCALHOST = "172.26.45.87"
-    PORT = 8080
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind((LOCALHOST, PORT))
+
     print("Server started")
     print("Waiting for client request")
     while True:
         server.listen(1)
         clientsock, client_address = server.accept()
-        newthread = ClientThread(client_address, clientsock)
+        print("\nConnection from : ", client_address)
+        newthread = ClientThread(clientsock)
         newthread.start()
 
 
-if __name__ == '__main__':
+# get these from .config file
+LOCALHOST = "172.26.45.87"
+PORT = 8080
+RECEIVE_BUFFER = 2048
+log_levels = {1: "DEBUG", 2: "INFO", 3: "WARN", 4: "ERROR", 5: "FATAL"}
+time_format = "%Y-%m-%d %H:%M:%S.%f"
+
+if __name__ == "__main__":
     main()
